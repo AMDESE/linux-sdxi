c4e38c9462fdf86a10a931ad51068e494534c5e8
Nathan Lynch <nathan.lynch@amd.com>
2026-03-12 12:04:54 -0500

    dmaengine: sdxi: hw: Enumerate descriptor types, subtypes
    Move them out of the descriptor union where they really only clutter
    things up.
    
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/hw.h
+++ drivers/dma/sdxi/hw.h
@@ -178,14 +178,11 @@ struct sdxi_desc {
 	static_assert(offsetof(struct tag_, csb_ptr) ==			\
 		      offsetof(struct sdxi_dsc_generic, csb_ptr))
 
-#define SDXI_DSC_OP_TYPE_DMAB 0x001
-#define SDXI_DSC_OP_SUBTYPE_NOP 0x01
 		/* SDXI 1.0 Table 6-6: DSC_DMAB_NOP Descriptor Format */
 		define_sdxi_dsc(sdxi_dsc_dmab_nop, nop,
 			__u8 rsvd_0[52];
 		);
 
-#define SDXI_DSC_OP_SUBTYPE_COPY 0x03
 		/* SDXI 1.0 Table 6-8: DSC_DMAB_COPY Descriptor Format */
 		define_sdxi_dsc(sdxi_dsc_dmab_copy, copy,
 			__le32 size;
@@ -198,8 +195,6 @@ struct sdxi_desc {
 			__u8 rsvd_1[24];
 		);
 
-#define SDXI_DSC_OP_TYPE_INTR 0x004
-#define SDXI_DSC_OP_SUBTYPE_INTR 0x00
 		/* SDXI 1.0 Table 6-12: DSC_INTR Descriptor Format */
 		define_sdxi_dsc(sdxi_dsc_intr, intr,
 			__u8 rsvd_0[8];
@@ -207,9 +202,6 @@ struct sdxi_desc {
 			__u8 rsvd_1[42];
 		);
 
-#define SDXI_DSC_OP_TYPE_ADMIN 0x002
-#define SDXI_DSC_OP_SUBTYPE_CXT_START_NM 0x03
-#define SDXI_DSC_OP_SUBTYPE_CXT_START_RS 0x08
 		/* SDXI 1.0 Table 6-14: DSC_CXT_START Descriptor Format */
 		define_sdxi_dsc(sdxi_dsc_cxt_start, cxt_start,
 			__u8 rsvd_0;
@@ -222,7 +214,6 @@ struct sdxi_desc {
 			__u8 rsvd_2[32];
 		);
 
-#define SDXI_DSC_OP_SUBTYPE_CXT_STOP     0x04
 		/* SDXI 1.0 Table 6-15: DSC_CXT_STOP Descriptor Format */
 		define_sdxi_dsc(sdxi_dsc_cxt_stop, cxt_stop,
 			__u8 rsvd_0;
@@ -236,7 +227,6 @@ struct sdxi_desc {
 #define SDXI_DSC_CXT_STOP_HS BIT(6)
 #define SDXI_DSC_CXT_STOP_VF BIT(8)
 
-#define SDXI_DSC_OP_SUBTYPE_SYNC 0x06
 		/* SDXI 1.0 Table 6-22: DSC_SYNC Descriptor Format */
 		define_sdxi_dsc(sdxi_dsc_sync, sync,
 			__u8 cflags;
@@ -255,5 +245,28 @@ struct sdxi_desc {
 	};
 } __packed;
 static_assert(sizeof(struct sdxi_desc) == 64);
+
+/* SDXI 1.0 Table 6-1: SDXI Operation Groups */
+enum sdxi_dsc_type {
+	SDXI_DSC_OP_TYPE_DMAB    = 0x001,
+	SDXI_DSC_OP_TYPE_ADMIN   = 0x002,
+	SDXI_DSC_OP_TYPE_INTR    = 0x004,
+};
+
+/* SDXI 1.0 Table 6-2: SDXI Operation Groups, Types, and Subtypes */
+enum sdxi_dsc_subtype {
+	/* DMA Base */
+	SDXI_DSC_OP_SUBTYPE_NOP     = 0x01,
+	SDXI_DSC_OP_SUBTYPE_COPY    = 0x03,
+
+	/* Interrupt */
+	SDXI_DSC_OP_SUBTYPE_INTR = 0x00,
+
+	/* Administrative */
+	SDXI_DSC_OP_SUBTYPE_CXT_START_NM = 0x03,
+	SDXI_DSC_OP_SUBTYPE_CXT_START_RS = 0x08,
+	SDXI_DSC_OP_SUBTYPE_CXT_STOP     = 0x04,
+	SDXI_DSC_OP_SUBTYPE_SYNC         = 0x06,
+};
 
 #endif /* DMA_SDXI_HW_H */
