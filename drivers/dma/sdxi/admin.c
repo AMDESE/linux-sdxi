81883c856823e92081f71f926eaf1ac3e5332bfb
Nathan Lynch <nathan.lynch@amd.com>
2026-03-12 12:21:51 -0500

    dmaengine: sdxi: admin: Prefer <linux/processor.h> [checkpatch]
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/admin.c
+++ drivers/dma/sdxi/admin.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include <linux/cleanup.h>
 #include <linux/kernel.h>
-#include <asm/processor.h>
+#include <linux/processor.h>
 #include <asm/rwonce.h>
 
 #include "admin.h"
