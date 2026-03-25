5b2283f7dc3999962494e4c69b7c722035f37b2e
Nathan Lynch <nathan.lynch@amd.com>
2026-03-25 16:44:44 -0500

    dmaengine: sdxi: Drop unneeded "context management" comment
    I think the member names make it clear enough what's going on, and
    there are other members related to context management elsewhere. Best
    to drop it.
    
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/sdxi.h
+++ drivers/dma/sdxi/sdxi.h
@@ -101,7 +101,6 @@ struct sdxi_dev {
 	u16 max_cxtid;			/* Maximum context ID allowed. */
 	u32 op_grp_cap;			/* supported operatation group cap */
 
-	/* context management */
 	struct sdxi_cxt_L2_table *L2_table;
 	dma_addr_t L2_dma;
 	struct sdxi_cxt_L1_table *L1_table;
