d762c1898e908eb797655074846afa737007a49b
Nathan Lynch <nathan.lynch@amd.com>
2026-03-27 11:15:22 -0500

    dmaengine: sdxi: Add client context alloc and release APIs
    Expose sdxi_cxt_new() and sdxi_cxt_exit(), which are the rest of the
    driver's entry points to creating and releasing SDXI contexts.
    
    Track client contexts in a device-wide allocating xarray, mapping
    context ID to the context object. The admin context always has ID 0,
    so begin allocations at 1. Define a local sdxi_cxt_id class to
    facilitate early allocation (before committing more resources) and
    automatic release of context IDs.
    
    Introduce new code to invalidate a context's entry in the L1 table on
    deallocation.
    
    Support for starting and stopping contexts will be added in changes to
    follow.
    
    The only expected user of sdxi_cxt_new() and sdxi_cxt_exit() at this
    point is the DMA engine provider code where a client context per
    channel will be created.
    
    Co-developed-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Nathan Lynch <nathan.lynch@amd.com>

--- drivers/dma/sdxi/context.h
+++ drivers/dma/sdxi/context.h
@@ -44,5 +44,17 @@ struct sdxi_cxt {
 };
 
 int sdxi_admin_cxt_init(struct sdxi_dev *sdxi);
+struct sdxi_cxt *sdxi_cxt_new(struct sdxi_dev *sdxi);
+void sdxi_cxt_exit(struct sdxi_cxt *cxt);
+
+static inline struct sdxi_cxt *to_admin_cxt(const struct sdxi_cxt *cxt)
+{
+	return cxt->sdxi->admin_cxt;
+}
+
+static inline bool sdxi_cxt_is_admin(const struct sdxi_cxt *cxt)
+{
+	return cxt == to_admin_cxt(cxt);
+}
 
 #endif /* DMA_SDXI_CONTEXT_H */
